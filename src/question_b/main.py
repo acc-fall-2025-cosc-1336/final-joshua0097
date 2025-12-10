be0d1822b3c04c0770beead9607476fa503f6aac
Joshua Alvarez <joshua.alvarez2@g.austincc.edu>
2025-12-10 00:59:37 +0000

    final progress 2

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,21 @@
-#add import
+from question_b import get_stock_list, display_stock_list
+
+def main():
+    stock_list = get_stock_list()
+    
+    while True:
+        print("\n1-Display stock purchase history")
+        print("2-Exit")
+        choice = input("\nEnter option: ")
+        
+        if choice == '1':
+            display_stock_list(stock_list)
+        elif choice == '2':
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+if __name__ == '__main__':
+    main()
+ 
+
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,26 @@
-#write functions here, don't add input('') statements here!
+    
+class Stock:
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+    
+    def get_symbol(self):
+        return self.__symbol
+    
+    def get_company_name(self):
+        return self.__company_name
+def get_stock_list():
+    stock_list = [
+        Stock('AAPL', 'Apple Inc'),
+        Stock('CAT', 'Caterpillar'),
+        Stock('EK', 'Eastman Kodak'),
+        Stock('GOOG', 'Google'),
+        Stock('MSFT', 'Microsoft')
+    ]
+    return stock_list
+
+def display_stock_list(stock_list):
+    print("\nStock Report")
+    print("Company Symbol")
+    for stock in stock_list:
+        print(f"{stock.get_company_name()} {stock.get_symbol()}")
